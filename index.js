3ed791d0e86a28d2a54c41ebe2206b4aa17a6175
subrotomojumder <97802263+subrotomojumder@users.noreply.github.com>
2022-10-19 10:33:59 +0600

    my first backend server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const categories = require('./data/categories.json');
+
+
+app.get('/', (req, res) => {
+    res.send('News Dragon server');
+})
+
+app.get('/category', (req, res)=> {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log('Dragon News server running on port', port)
+})
